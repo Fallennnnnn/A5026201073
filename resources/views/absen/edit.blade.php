55a402ed56a80159c2d33d0fea042a74794b7db4
Ega Fernanda Putra <dragoon.fernanda12@gmail.com>
2021-12-06 12:44:30 +0700

    Update edit.blade.php

--- resources/views/absen/edit.blade.php
+++ resources/views/absen/edit.blade.php
@@ -2,23 +2,22 @@
    @extends('layout.bahagia')
 
    @section('title', 'Mengedit Data Absensi')
-   @section('judulhalaman', 'Edit Absensi Pegawai')
+   @section('judulhalaman', 'Edit Data Absensi Pegawai')
 
    @section('konten')
 
-<h3>{{ $judul }}</h3>
 	@foreach($absen as $a)
 	<form action="/absen/update" method="post">
 		{{ csrf_field() }}
 		<input type="hidden" name="id" value="{{ $a->ID }}">
-        IDPegawai <select name="idpegawai" >
+        <label for ="IDPegawai">Pegawai</label> <select name="idpegawai" class = "form-control" >
             @foreach($pegawai as $p)
              <option value="{{ $p->pegawai_id }}" @if ($p->pegawai_id === $a->IDPegawai ) selected="selected" @endif>{{ $p->pegawai_nama }}</option>
             @endforeach
         </select>
         <br>
                 <div class="form-group">
-                    <label for="dtpickerdemo" class="col-sm-2 control-label">Tanggal :</label>
+                    <label for="dtpickerdemo" class="control-label">Tanggal :</label>
                     <div class='col-sm-4 input-group date ' id='dtpickerdemo'>
                         <input type='text' class="form-control" name="tanggal" value="{{ $a->Tanggal }}" required="required" />
                         <span class="input-group-addon">
@@ -37,16 +36,19 @@
                     });
                 </script>
 
-                Status <br />
+                <label for = "select-status">Status</label><br>
+
                 <input type="radio" id="html" name="status" value="I" @if ($a->Status === "I" ) checked="checked" @endif>
                 <label for="html">Izin</label><br>
                 <input type="radio" id="css" name="status" value="S"  @if ($a->Status === "S" ) checked="checked" @endif>
                 <label for="css">Sakit</label><br>
                 <input type="radio" id="javascript" name="status" value="A"  @if ($a->Status === "A" ) checked="checked" @endif>
-                <label for="javascript">Alpha</label>
-        <p><input type="submit" value="Simpan Data"></p>
+                <label for="javascript">Alpha</label><br><br>
+
+        <input type="submit" class = "btn btn-success" value="Simpan Data">
+         <a href="/absen" class = "btn btn-primary"> Kembali </a>
+         <br><br>
 	</form>
 	@endforeach
 
-   <p> <a href="/absen"> Kembali</a></p>
    @endsection
