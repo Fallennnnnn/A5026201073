cadb6e6a1a1792346d30317b641e9a4bd116450b
Ega Fernanda Putra <dragoon.fernanda12@gmail.com>
2021-12-06 12:44:49 +0700

    Update tambah.blade.php

--- resources/views/pendapatan/tambah.blade.php
+++ resources/views/pendapatan/tambah.blade.php
@@ -1,27 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tugas PWEB Query Builder</title>
-</head>
+@extends('layout.bahagia')
+
+@section('title', 'Menambah Data Pendapatan Pegawai')
+@section('judulhalaman', 'Tambah Data Pendapatan Pegawai')
+
+@section('konten')
 <body>
 
-    <h3>Tambah Data Pendapatan</h3>
-
-	<a href="/pendapatan"> Kembali</a>
-
-	<br/>
-	<br/>
 
 	<form action="/pendapatan/store" method="post">
 		{{ csrf_field() }}
-		IDPegawai <input type="number" name="idpegawai" required="required"> <br/>
-		Bulan<input type="number" name="bulan" required="required"> <br/>
-        Tahun<input type="number" name="tahun" required="required"> <br/>
-        Gaji<input type="number" name="gaji" required="required"> <br/>
-        Tunjangan<input type="number" name="tunjangan" required="required"> <br/>
+        <div class="form-group">
+		<label for ="idpegawai">ID Pegawai <input type="number" name="idpegawai" required="required" class = "form-control"> <br/>
+		<label for = "bulan">Bulan<input type="number" name="bulan" required="required" class = "form-control"> <br/>
+        <label for = "tahun">Tahun<input type="number" name="tahun" required="required" class = "form-control"> <br/>
+        <label for = "gaji">Gaji<input type="number" name="gaji" required="required" class = "form-control"> <br/>
+        <label for = "tunjangan">Tunjangan<input type="number" name="tunjangan" required="required" class = "form-control"> <br/>
 
-		<input type="submit" value="Simpan Data">
+		<input type="submit" value="Simpan Data" class = "btn btn-success">
+        <a href="/pendapatan" class = "btn btn-primary"> Kembali</a>
+    </div>
 	</form>
 
+
 </body>
-</html>
+@endsection
