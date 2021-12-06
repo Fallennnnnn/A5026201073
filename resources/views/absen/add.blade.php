96ab6eed6d4c637b0c7989980e75ef90a6f1819b
Ega Fernanda Putra <dragoon.fernanda12@gmail.com>
2021-12-06 12:44:27 +0700

    Update add.blade.php

--- resources/views/absen/add.blade.php
+++ resources/views/absen/add.blade.php
@@ -2,20 +2,21 @@
    @extends('layout.bahagia')
 
    @section('title', 'Menambah Absen')
-   @section('judulhalaman', 'Menambah Data Absen')
+   @section('judulhalaman', 'Menambah Data Absen Pegawai')
 
    @section('konten')
+
     <form action="/absen/store" method="post">
         {{ csrf_field() }}
-
-        IDPegawai <select name="idpegawai" >
+        <label for = "idpegawai">
+        Pegawai</label> <select class = "form-control" name="idpegawai" >
     @foreach($pegawai as $p)
      <option value="{{ $p->pegawai_id }}">{{ $p->pegawai_nama }}</option>
     @endforeach
 </select>
 <br>
         <div class="form-group">
-            <label for="dtpickerdemo" class="col-sm-2 control-label">Tanggal :</label>
+            <label for="dtpickerdemo" class="control-label">Tanggal :</label>
             <div class='col-sm-4 input-group date ' id='dtpickerdemo'>
                 <input type='text' class="form-control" name="tanggal" required="required" />
                 <span class="input-group-addon">
@@ -26,7 +27,7 @@
         <script type="text/javascript">
             $(function() {
                 $('#dtpickerdemo').datetimepicker({
-                    format: 'YYYY-MM-DD HH:mm:ss',
+                    format: 'YYYY-MM-DD hh:mm:ss',
                     showTodayButton: false,
                     locale : 'id',
                     "defaultDate": new Date(),
@@ -34,15 +35,18 @@
             });
         </script>
 
-        Status <br />
+       <label for ="select-status"> Status</label> <br/>
+
         <input type="radio" id="html" name="status" value="I">
         <label for="html">Izin</label><br>
         <input type="radio" id="css" name="status" value="S" checked="checked">
         <label for="css">Sakit</label><br>
         <input type="radio" id="javascript" name="status" value="A">
-        <label for="javascript">Alpha</label>
+        <label for="javascript">Alpha</label><br><br>
 
-        <p><input type="submit" value="Simpan Data"></p>
+        <input type="submit" class= "btn btn-success" value="Simpan Data">
+        <a href="/absen" class = "btn btn-primary"> Kembali</a>
     </form>
-    <p> <a href="/absen"> Kembali</a></p>
+    <br>
+
 @endsection
