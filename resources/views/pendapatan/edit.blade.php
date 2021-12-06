b12c7e32998a8d7ad938eb528c1c197c1f9ba1e0
Ega Fernanda Putra <dragoon.fernanda12@gmail.com>
2021-12-06 12:44:53 +0700

    Update edit.blade.php

--- resources/views/pendapatan/edit.blade.php
+++ resources/views/pendapatan/edit.blade.php
@@ -1,31 +1,29 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tugas PWEB Query Builder</title>
-</head>
+@extends('layout.bahagia')
+
+@section('title', 'Mengedit Data Pendapatan Pegawai')
+@section('judulhalaman', 'Edit Data Pendapatan Pegawai')
+
+@section('konten')
 <body>
 
-    <h3>Edit Data Pendapatan</h3>
-
-	<a href="/pendapatan"> Kembali</a>
-
-	<br/>
-	<br/>
 
 	@foreach($pendapatan as $p)
 	<form action="/pendapatan/update" method="post">
 		{{ csrf_field() }}
+        <div class="form-group">
 		<input type="hidden" name="id" value="{{ $p->ID }}"> <br/>
-		IDPegawai <input type="number" required="required" name="idpegawai" value="{{ $p->IDPegawai }}"> <br/>
-		Bulan <input type="number" required="required" name="bulan" value="{{ $p->Bulan }}"> <br/>
-		Tahun <input type="number" required="required" name="tahun" value="{{ $p->Tahun }}"> <br/>
-        Gaji <input type="number" required="required" name="gaji" value="{{ $p->Gaji }}"> <br/>
-        Tunjangan <input type="number" required="required" name="tunjangan" value="{{ $p->Tunjangan }}"> <br/>
+        <label for ="idpegawai">IDPegawai</label> <input type="number" required="required" name="idpegawai" value="{{ $p->IDPegawai }}" class = "form-control"> <br/>
+		<label for = "bulan">Bulan <input type="number" required="required" name="bulan" value="{{ $p->Bulan }}" class = "form-control"> <br/>
+		<label for = "tahun">Tahun <input type="number" required="required" name="tahun" value="{{ $p->Tahun }}" class = "form-control"> <br/>
+        <label for = "gaji">Gaji <input type="number" required="required" name="gaji" value="{{ $p->Gaji }}" class = "form-control"> <br/>
+        <label for = "tunjangan">Tunjangan <input type="number" required="required" name="tunjangan" value="{{ $p->Tunjangan }}" class = "form-control"> <br/>
 
-		<input type="submit" value="Simpan Data">
+		<input type="submit" value="Simpan Data" class = "btn btn-success">
+        <a href="/pendapatan" class = "btn btn-primary"> Kembali</a>
+    </div>
 	</form>
 	@endforeach
 
 
 </body>
-</html>
+@endsection
