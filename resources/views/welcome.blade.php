d0101261128a0c0f13b117be2999fdc899b2dc3c
Ega Fernanda Putra <dragoon.fernanda12@gmail.com>
2021-12-06 12:48:07 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -94,6 +94,7 @@
                     <a href="praktikum2">Praktikum 2</a>
                     <a href="formets">ETS</a>
                     <a href="smallnumfinder">Tugas PHP</a>
+                    <a href="/pegawai">CRUD Pegawai</a>
                 </div>
             </div>
         </div>
